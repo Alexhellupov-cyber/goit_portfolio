dc96894150efcda82e347463c0d154b89252302b
Eugene <y.holubnychyi@gmail.com>
2025-03-08 20:22:17 -0330

    COVERS_BETA2

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import './js/covers.js';
+import './js/reviews.js';
+import './js/benefits.js'
+import './js/faq.js'
+import './js/header.js'
+import './js/hero.js'
+import './js/projects.js'
+import './js/work-together.js'
+import './js/about-me.js'
+import './js/about-me-2.js'
