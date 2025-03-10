6fa6670ee321306806a039f4dd9de2934c7e78c1
HnatenkoOlya <gnatenkoolgaig1210@gmail.com>
2025-03-10 22:39:02 +0100

    projects/2

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,23 +1,38 @@
-import Swiper from 'swiper';
-import 'swiper/css';
-document.addEventListener('DOMContentLoaded', () => {
-  const swiper = new Swiper('.projects-slider', {
-    loop: true,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    slidesPerView: 1,
-    spaceBetween: 20,
-    breakpoints: {
-      768: { slidesPerView: 2 },
-      1024: { slidesPerView: 3 },
-    },
-  });
-
-  console.log('Swiper instance created', swiper);
-});
+    const swiper = new Swiper('.projects-slider', {
+        loop: true,
+        slidesPerView: 1,
+        spaceBetween: 20,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        keyboard: {
+            enabled: true,
+            onlyInViewport: false,
+        },
+        breakpoints: {
+            768: {
+                slidesPerView: 2,
+            },
+            1024: {
+                slidesPerView: 3,
+            },
+        },
+    });
+    
+    swiper.on('slideChange', function () {
+        const prevButton = document.querySelector('.swiper-button-prev');
+        const nextButton = document.querySelector('.swiper-button-next');
+        
+        if (swiper.isBeginning) {
+            prevButton.disabled = true;
+        } else {
+            prevButton.disabled = false;
+        }
+    
+        if (swiper.isEnd) {
+            nextButton.disabled = true;
+        } else {
+            nextButton.disabled = false;
+        }
+    });
