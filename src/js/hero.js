4f9b7063feeb5a9207634cfd30f5a8aef14ae74a
DemonVenom666 <demonvenom9@gmail.com>
2025-03-11 01:33:32 +0100

    update

--- src/js/hero.js
+++ src/js/hero.js
@@ -5,26 +5,21 @@ document.addEventListener('DOMContentLoaded', () => {
     const themeRedBtn = document.querySelector('[data-theme-red]');
     const themeGreenBtn = document.querySelector('[data-theme-green]');
 
-    // Отримуємо збережену тему або ставимо red за замовчуванням
     let savedTheme = localStorage.getItem('selectedTheme') || 'theme-red';
     applyTheme(savedTheme);
 
     function applyTheme(theme) {
-        // Видаляємо всі теми перед додаванням нової
         hero.classList.remove('theme-red', 'theme-green');
         themeButtons.forEach(button => button.classList.remove('theme-red', 'theme-green'));
         themeTexts.forEach(text => text.classList.remove('theme-red', 'theme-green'));
 
-        // Додаємо нову тему
         hero.classList.add(theme);
         themeButtons.forEach(button => button.classList.add(theme));
         themeTexts.forEach(text => text.classList.add(theme));
 
-        // Зберігаємо тему в localStorage
         localStorage.setItem('selectedTheme', theme);
     }
 
-    // Кнопки зміни теми
     themeRedBtn.addEventListener('click', () => applyTheme('theme-red'));
     themeGreenBtn.addEventListener('click', () => applyTheme('theme-green'));
 });
